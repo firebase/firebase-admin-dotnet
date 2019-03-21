97196598bb0cd88c6db5e66783b67fcc09b67ad0
Kent Boogaart <kent.boogaart@gmail.com>
2019-03-21 10:57:08 +1000

    Add BatchResponse class

---
+++ FirebaseAdmin/FirebaseAdmin.Tests/Messaging/BatchResponseTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FirebaseAdmin.Messaging;
+using Xunit;
+
+namespace FirebaseAdmin.Tests.Messaging
+{
+    public class BatchResponseTest
+    {
+        [Fact]
+        public void EmptyResponses()
+        {
+            var responses = new List<SendResponse>();
+
+            var batchResponse = new BatchResponse(responses);
+
+            Assert.Equal(0, batchResponse.SuccessCount);
+            Assert.Equal(0, batchResponse.FailureCount);
+            Assert.Equal(0, batchResponse.Responses.Count);
+        }
+
+        [Fact]
+        public void SomeResponse()
+        {
+            var responses = new SendResponse[]
+            {
+                SendResponse.FromMessageId("message1"),
+                SendResponse.FromMessageId("message2"),
+                SendResponse.FromException(
+                    new FirebaseMessagingException(
+                        "error-code",
+                        "error-message",
+                        null)),
+            };
+
+            var batchResponse = new BatchResponse(responses);
+
+            Assert.Equal(2, batchResponse.SuccessCount);
+            Assert.Equal(1, batchResponse.FailureCount);
+            Assert.Equal(3, batchResponse.Responses.Count);
+            Assert.True(responses.SequenceEqual(batchResponse.Responses));
+        }
+
+        [Fact]
+        public void ResponsesCannotBeNull()
+        {
+            Assert.Throws<ArgumentNullException>(() => new BatchResponse(null));
+        }
+    }
+}
---
+++ FirebaseAdmin/FirebaseAdmin/Messaging/BatchResponse.cs
@@ -0,0 +1,69 @@
+﻿// Copyright 2018, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+
+namespace FirebaseAdmin.Messaging
+{
+    /// <summary>
+    /// Response from an operation that sends FCM messages to multiple recipients.
+    /// See <see cref="FirebaseMessaging.SendMulticastAsync(MulticastMessage)"/>.
+    /// </summary>
+    public sealed class BatchResponse
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BatchResponse"/> class.
+        /// </summary>
+        /// <param name="responses">The responses.</param>
+        public BatchResponse(IEnumerable<SendResponse> responses)
+        {
+            if (responses == null)
+            {
+                throw new ArgumentNullException(nameof(responses));
+            }
+
+            this.Responses = responses.Copy();
+
+            var successCount = 0;
+
+            foreach (var response in responses)
+            {
+                if (response.IsSuccessful)
+                {
+                    ++successCount;
+                }
+            }
+
+            this.SuccessCount = successCount;
+        }
+
+        /// <summary>
+        /// Gets information about all responses for the batch.
+        /// </summary>
+        public IReadOnlyList<SendResponse> Responses { get; }
+
+        /// <summary>
+        /// Gets a count of how many of the responses in <see cref="Responses"/> were
+        /// successful.
+        /// </summary>
+        public int SuccessCount { get; }
+
+        /// <summary>
+        /// Gets a count of how many of the responses in <see cref="Responses"/> were
+        /// unsuccessful.
+        /// </summary>
+        public int FailureCount => this.Responses.Count - this.SuccessCount;
+    }
+}
