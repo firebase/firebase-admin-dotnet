10c0b1658dce54e21a03dc5f433d11708bc13050
Artemy <mr.lolnub@gmail.com>
2019-07-23 10:31:51 +0500

    Added documentation headers

--- FirebaseAdmin/FirebaseAdmin.Tests/Messaging/FcmOptionsTest.cs
+++ FirebaseAdmin/FirebaseAdmin.Tests/Messaging/FcmOptionsTest.cs
@@ -1,3 +1,17 @@
+// Copyright 2019, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System;
 using FirebaseAdmin.Messaging;
 using FirebaseAdmin.Messaging.Util;
@@ -35,13 +49,20 @@ namespace FirebaseAdmin.Tests.Messaging
         public void FcmOptionsCopyAndValidateNullLabel()
         {
             var options = new FcmOptions() { AnalyticsLabel = null };
+            options.CopyAndValidate();
+        }
+
+        [Fact]
+        public void FcmOptionsCopyAndValidateEmptyLabel()
+        {
+            var options = new FcmOptions() { AnalyticsLabel = string.Empty };
             Assert.Throws<ArgumentException>(() => options.CopyAndValidate());
         }
 
         [Fact]
         public void AnalyticsLabelTooLong()
         {
-            Assert.Throws<ArgumentException>(() => AnalyticsLabelChecker.ValidateAnalyticsLabel("012345678901234567890123456789012345678901234567890"));
+            Assert.Throws<ArgumentException>(() => AnalyticsLabelChecker.ValidateAnalyticsLabel(new string('a', 51)));
         }
 
         [Fact]
--- FirebaseAdmin/FirebaseAdmin/Messaging/Util/AnalyticsLabelChecker.cs
+++ FirebaseAdmin/FirebaseAdmin/Messaging/Util/AnalyticsLabelChecker.cs
@@ -1,3 +1,17 @@
+// Copyright 2019, Google Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 using System;
 using System.Text.RegularExpressions;
 
@@ -17,7 +31,12 @@ namespace FirebaseAdmin.Messaging.Util
         /// <param name="analyticsLabel">Analytics label.</param>
         public static void ValidateAnalyticsLabel(string analyticsLabel)
         {
-            if (string.IsNullOrWhiteSpace(analyticsLabel))
+            if (analyticsLabel == null)
+            {
+                return;
+            }
+
+            if (analyticsLabel == string.Empty)
             {
                 throw new ArgumentException("Analytics label must have format matching'^[a-zA-Z0-9-_.~%]{1,50}$");
             }
