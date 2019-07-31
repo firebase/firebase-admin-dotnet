b22734a1ee44d94f33e019d737a6cf3619e7fa1a
Leo <leo@tournament.gg>
2019-07-31 18:22:02 +0100

    HTTP status code tests

--- FirebaseAdmin/FirebaseAdmin.Tests/Messaging/InstanceIdClientTest.cs
+++ FirebaseAdmin/FirebaseAdmin.Tests/Messaging/InstanceIdClientTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading.Tasks;
 using FirebaseAdmin.Messaging;
 using Google.Apis.Auth.OAuth2;
@@ -59,6 +60,86 @@ namespace FirebaseAdmin.Tests.Messaging
             var result = await client.UnsubscribeFromTopicAsync("test-topic", new List<string> { "abc123" });
 
             Assert.Equal(1, result.SuccessCount);
+        }
+
+        [Fact]
+        public async Task BadRequest()
+        {
+            var handler = new MockMessageHandler()
+            {
+                StatusCode = HttpStatusCode.BadRequest,
+                Response = "BadRequest",
+            };
+            var factory = new MockHttpClientFactory(handler);
+
+            var client = new InstanceIdClient(factory, MockCredential);
+
+            await Assert.ThrowsAsync<FirebaseMessagingException>(
+               () => client.SubscribeToTopicAsync("test-topic", new List<string> { "abc123" }));
+        }
+
+        [Fact]
+        public async Task Unauthorized()
+        {
+            var handler = new MockMessageHandler()
+            {
+                StatusCode = HttpStatusCode.Unauthorized,
+                Response = "Unauthorized",
+            };
+            var factory = new MockHttpClientFactory(handler);
+
+            var client = new InstanceIdClient(factory, MockCredential);
+
+            await Assert.ThrowsAsync<FirebaseMessagingException>(
+               () => client.SubscribeToTopicAsync("test-topic", new List<string> { "abc123" }));
+        }
+
+        [Fact]
+        public async Task Forbidden()
+        {
+            var handler = new MockMessageHandler()
+            {
+                StatusCode = HttpStatusCode.Forbidden,
+                Response = "Forbidden",
+            };
+            var factory = new MockHttpClientFactory(handler);
+
+            var client = new InstanceIdClient(factory, MockCredential);
+
+            await Assert.ThrowsAsync<FirebaseMessagingException>(
+               () => client.SubscribeToTopicAsync("test-topic", new List<string> { "abc123" }));
+        }
+
+        [Fact]
+        public async Task NotFound()
+        {
+            var handler = new MockMessageHandler()
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Response = "NotFound",
+            };
+            var factory = new MockHttpClientFactory(handler);
+
+            var client = new InstanceIdClient(factory, MockCredential);
+
+            await Assert.ThrowsAsync<FirebaseMessagingException>(
+               () => client.SubscribeToTopicAsync("test-topic", new List<string> { "abc123" }));
+        }
+
+        [Fact]
+        public async Task ServiceUnavailable()
+        {
+            var handler = new MockMessageHandler()
+            {
+                StatusCode = HttpStatusCode.ServiceUnavailable,
+                Response = "ServiceUnavailable",
+            };
+            var factory = new MockHttpClientFactory(handler);
+
+            var client = new InstanceIdClient(factory, MockCredential);
+
+            await Assert.ThrowsAsync<FirebaseMessagingException>(
+               () => client.SubscribeToTopicAsync("test-topic", new List<string> { "abc123" }));
         }
     }
 }
