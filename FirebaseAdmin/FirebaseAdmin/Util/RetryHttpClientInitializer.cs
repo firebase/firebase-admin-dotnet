b0b462ac250516751721e85cf488e67d928e00b4
hiranya911 <hiranya911@gmail.com>
2019-09-19 17:12:24 -0700

    Simplified how the back off handler is registered

--- FirebaseAdmin/FirebaseAdmin/Util/RetryHttpClientInitializer.cs
+++ FirebaseAdmin/FirebaseAdmin/Util/RetryHttpClientInitializer.cs
@@ -18,41 +18,25 @@ using Google.Apis.Util;
 namespace FirebaseAdmin.Util
 {
     /// <summary>
-    /// An HTTP client initializer that configures clients to retry HTTP failing requests on
+    /// An HTTP client initializer that configures clients to retry failing HTTP requests on
     /// low-level exceptions and unsuccessful HTTP responses. Retry conditions and other parameters
-    /// are configured via <see cref="RetryOptions"/>. Repeated retry attempts on the same
-    /// request are delayed with exponential backoff. Retries due to unsuccessful responses
-    /// that contain the "Retry-After" header are delayed according to the header value.
+    /// are configured via <see cref="RetryOptions"/>. Supports exponential back-off and the
+    /// "Retry-After" header in error responses.
     /// </summary>
     internal sealed class RetryHttpClientInitializer : IConfigurableHttpClientInitializer
     {
         private readonly RetryOptions retryOptions;
-        private readonly ExponentialBackOffInitializer backOffInitializer;
 
         internal RetryHttpClientInitializer(RetryOptions retryOptions)
         {
             this.retryOptions = retryOptions.ThrowIfNull(nameof(retryOptions));
-            var backOffHandler = new FirebaseBackOffHandler(this.retryOptions);
-
-            // ExponentialBackOffPolicy.Exception:
-            // Instructs the initializer to register an exception handler with the underlying
-            // ConfigurableMessageHandler. Whether to retry on a certain exception or not is
-            // determined by the BackOffHandler.HandleExceptionFunc.
-
-            // ExponentialBackOffPolicy.UnsuccessfulResponse503:
-            // This constant is poorly named. It simply instructs the initializer to
-            // register an unsuccessful HTTP response handler with the underlying
-            // ConfigurableMessageHandler. Whether a certain response is retried or not is
-            // determined by the BackOffHandler.HandleUnsuccessfulResponseFunc.
-            this.backOffInitializer = new ExponentialBackOffInitializer(
-                ExponentialBackOffPolicy.Exception |
-                ExponentialBackOffPolicy.UnsuccessfulResponse503,
-                () => backOffHandler);
         }
 
         public void Initialize(ConfigurableHttpClient client)
         {
-            this.backOffInitializer.Initialize(client);
+            var backOffHandler = new FirebaseBackOffHandler(this.retryOptions);
+            client.MessageHandler.AddExceptionHandler(backOffHandler);
+            client.MessageHandler.AddUnsuccessfulResponseHandler(backOffHandler);
 
             // NumTries is the global setting that controls how many times the
             // ConfigurableMessageHandler is willing to retry a request. This defaults to 3 in the
