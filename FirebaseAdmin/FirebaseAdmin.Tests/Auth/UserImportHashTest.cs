260a2c59b60f117ff895f340a905ff47af841ac6
levimatheri <levimatheri@gmail.com>
2019-12-21 21:51:14 -0500

    Add UserImportHash and its corresponding test class

---
+++ FirebaseAdmin/FirebaseAdmin.Tests/Auth/UserImportHashTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FirebaseAdmin.Auth;
+using Xunit;
+
+namespace FirebaseAdmin.Tests.Auth
+{
+    public class UserImportHashTest
+    {
+        private static readonly byte[] SIGNERKEY = Encoding.ASCII.GetBytes("key");
+        private static readonly byte[] SALTSEPARATOR = Encoding.ASCII.GetBytes("separator");
+
+        [Fact]
+        public void TestBase()
+        {
+            var hash = new MockHash();
+            Assert.Equal(
+                new Dictionary<string, object>
+                {
+                    { "hashAlgorithm", "MOCK_HASH" },
+                    { "key", "value" },
+                }, hash.Properties());
+        }
+
+        private class MockHash : UserImportHash
+        {
+            internal MockHash()
+                : base("MOCK_HASH") { }
+
+            protected override IReadOnlyDictionary<string, object> Options()
+            {
+                return new Dictionary<string, object> { { "key", "value" } };
+            }
+        }
+    }
+}
---
+++ FirebaseAdmin/FirebaseAdmin/Auth/UserImportHash.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirebaseAdmin.Auth
+{
+    /// <summary>
+    /// Represents a hash algorithm and the related configuration parameters used to hash user
+    /// passwords.An instance of this class must be specified if importing any users with password hashes.
+    /// </summary>
+    public abstract class UserImportHash
+    {
+        private readonly string name;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserImportHash"/> class.
+        /// </summary>
+        /// <param name="name">The name of the hashing algorithm.</param>
+        protected UserImportHash(string name)
+        {
+            this.name = name;
+        }
+
+        /// <summary>
+        /// Builds the configuration properties of the related algorithm used to hash user passwords.
+        /// </summary>
+        /// <returns>A dictionary containing the hash configurations.</returns>
+        public IReadOnlyDictionary<string, object> Properties()
+        {
+            var properties = new Dictionary<string, object>
+            {
+                { "hashAlgorithm", this.name },
+            };
+            foreach (var entry in this.Options())
+            {
+                properties.Add(entry.Key, entry.Value);
+            }
+
+            return properties;
+        }
+
+        /// <summary>
+        /// Creates a dictionary containing additional options related to the hash configurations.
+        /// </summary>
+        /// <returns>A dictionary with additional options related to the hash configurations.</returns>
+        protected abstract IReadOnlyDictionary<string, object> Options();
+    }
+}
