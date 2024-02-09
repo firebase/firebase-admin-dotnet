5df71464928f536ee2a715f0d2512fb529dbf6ec
Jonathan Edey <jedey@google.com>
2024-02-09 12:57:06 -0500

    Refactor build process and test multiple frameworks

--- .github/scripts/builder.sh
+++ .github/scripts/builder.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-# BUILD_CONFIGURATION=Release
-BUILD_CONFIGURATION=Debug
+BUILD_CONFIGURATION=Release
 
 dotnet restore FirebaseAdmin/FirebaseAdmin.sln
 dotnet build FirebaseAdmin/FirebaseAdmin.sln --configuration $BUILD_CONFIGURATION --no-restore
 dotnet pack FirebaseAdmin/FirebaseAdmin.sln --configuration $BUILD_CONFIGURATION --no-restore
-
-dotnet test FirebaseAdmin/FirebaseAdmin.Tests --no-build --framework netcoreapp3.1 --configuration $BUILD_CONFIGURATION
-dotnet test FirebaseAdmin/FirebaseAdmin.Tests --no-build --framework net6.0 --configuration $BUILD_CONFIGURATION
