e59dfadbcf67a8d845cab8d5d35c0a25995d9f54
Jonathan Edey <jedey@google.com>
2024-02-12 10:14:12 -0500

    Refactor and update nightly and release workflows

--- .github/scripts/builder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-BUILD_CONFIGURATION=Release
-
-dotnet restore FirebaseAdmin/FirebaseAdmin.sln
-dotnet build FirebaseAdmin/FirebaseAdmin.sln --configuration $BUILD_CONFIGURATION --no-restore
-dotnet pack FirebaseAdmin/FirebaseAdmin.sln --configuration $BUILD_CONFIGURATION --no-restore
