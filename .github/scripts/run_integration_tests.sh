bf69b912e1155cd2cb5688b52300d8240e07b496
Jonathan Edey <jedey@google.com>
2024-02-26 10:27:07 -0500

    Update workflows

--- .github/scripts/run_integration_tests.sh
+++ .github/scripts/run_integration_tests.sh
@@ -25,5 +25,6 @@ gpg --quiet --batch --yes --decrypt --passphrase="${FIREBASE_SERVICE_ACCT_KEY}" 
 
 echo "${FIREBASE_API_KEY}" > FirebaseAdmin/FirebaseAdmin.IntegrationTests/resources/integration_apikey.txt
 
-dotnet test FirebaseAdmin/FirebaseAdmin.IntegrationTests --framework netcoreapp3.1 --configuration Release
-dotnet test FirebaseAdmin/FirebaseAdmin.IntegrationTests --framework net6.0 --configuration Release
+dotnet test FirebaseAdmin/FirebaseAdmin.IntegrationTests --configuration Release --no-restore --no-build --framework netcoreapp3.1
+dotnet test FirebaseAdmin/FirebaseAdmin.IntegrationTests --configuration Release --no-restore --no-build --framework net462
+dotnet test FirebaseAdmin/FirebaseAdmin.IntegrationTests --configuration Release --no-restore --no-build --framework net6.0
