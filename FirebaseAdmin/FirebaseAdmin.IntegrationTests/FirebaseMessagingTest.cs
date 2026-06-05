c210002ac92c2afa95131e4705a1c2af4313ecf0
Yvonne Pan <103622026+yvonnep165@users.noreply.github.com>
2026-06-05 11:19:57 -0700

    Add integration tests

--- FirebaseAdmin/FirebaseAdmin.IntegrationTests/FirebaseMessagingTest.cs
+++ FirebaseAdmin/FirebaseAdmin.IntegrationTests/FirebaseMessagingTest.cs
@@ -136,6 +136,35 @@ namespace FirebaseAdmin.IntegrationTests
         }
 
         [Fact]
+        public async Task SendEachForMulticastFids()
+        {
+            var multicastMessage = new MulticastMessage
+            {
+                Notification = new Notification()
+                {
+                    Title = "Title",
+                    Body = "Body",
+                },
+                Android = new AndroidConfig()
+                {
+                    Priority = Priority.Normal,
+                    TimeToLive = TimeSpan.FromHours(1),
+                    RestrictedPackageName = "com.google.firebase.testing",
+                },
+                Fids = new[]
+                {
+                    "fid1",
+                    "fid2",
+                },
+            };
+            var response = await FirebaseMessaging.DefaultInstance.SendEachForMulticastAsync(multicastMessage, dryRun: true);
+            Assert.NotNull(response);
+            Assert.Equal(2, response.FailureCount);
+            Assert.Equal(MessagingErrorCode.Unregistered, response.Responses[0].Exception.MessagingErrorCode);
+            Assert.Equal(MessagingErrorCode.Unregistered, response.Responses[1].Exception.MessagingErrorCode);
+        }
+
+        [Fact]
         public async Task SubscribeToTopic()
         {
             var response = await FirebaseMessaging.DefaultInstance.SubscribeToTopicAsync(
